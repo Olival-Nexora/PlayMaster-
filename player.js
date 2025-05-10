99840879dbe5d4fcb5077efcef4cce22174c9a40
Olival-Nexora <oeamaquinas@gmail.com>
2025-05-10 15:33:14 -0300

    Add files via upload

---
+++ player.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const player = document.getElementById("player");
+  const m3uUrl = "http://play.biturl.vip/get.php?username=Playmaster&password=e6fx7fmxp3l&type=m3u_plus&output=ts";
+  player.src = m3uUrl;
+});
