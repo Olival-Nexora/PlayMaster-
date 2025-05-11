9fc0df67e175caf4dc9afc31520834fa366231e0
Olival-Nexora <oeamaquinas@gmail.com>
2025-05-11 09:33:23 -0300

    player.js

--- player.js
+++ player.js
@@ -1,6 +1,4 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  const player = document.getElementById("player");
-  const m3uUrl = "http://play.biturl.vip/get.php?username=Playmaster&password=e6fx7fmxp3l&type=m3u_plus&output=ts";
-  player.src = m3uUrl;
-});
+setTimeout(() => {
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("playerContainer").style.display = "block";
+}, 5000); // 5 segundos de splash screen
